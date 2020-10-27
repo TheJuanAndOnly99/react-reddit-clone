453ab107e4867cff59f7ead2dd9df996fc187032
Juan Estrella <juan.estrella137@gmail.com>
2020-10-28 00:01:11 +0100

    Configure MikroORM and PostgreSQL

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: [Post],
+    dbName: 'redditDB',
+    type: 'postgresql',
+    debug: !__prod__,
+  });
+  
+  const post = orm.em.create(Post, { title: 'my first post' });
+  await orm.em.persistAndFlush(post);
+  console.log('------------sql 2-------------');
+  await orm.em.nativeInsert(Post, { title: 'my second post' });
+};
+
+main().catch((err) => {
+  console.log(err);
+});
+
+
 console.log('Hello World')
