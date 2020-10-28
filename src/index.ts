e9910dfce4f70fc4595ba58c7812dca67d4971cc
Juan Estrella <juan.estrella137@gmail.com>
2020-10-28 13:32:45 +0100

    Mikro-orm with postgreSQL setup

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,20 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config"
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    entities: [Post],
-    dbName: 'redditDB',
-    type: 'postgresql',
-    debug: !__prod__,
-  });
-  
-  const post = orm.em.create(Post, { title: 'my first post' });
-  await orm.em.persistAndFlush(post);
-  console.log('------------sql 2-------------');
-  await orm.em.nativeInsert(Post, { title: 'my second post' });
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+  // const post = orm.em.create(Post, { title: "my first post" });
+  // await orm.em.persistAndFlush(post);
+
+  const posts = await orm.em.find(Post, {});
+  console.log(posts);
 };
 
 main().catch((err) => {
   console.log(err);
 });
-
 
 console.log('Hello World')
